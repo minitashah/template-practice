0ebc204754ac129cfcb96521ca320e601b5c3fb0
minita shah <shahminita3158@gmail.com>
2017-02-15 22:22:05 +0530

    create new repo

---
+++ include/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Grocery</title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css.map">
+  <link rel="stylesheet" type="text/css" href="assets/css/min.css">
+<!--   <link rel="stylesheet" type="text/css" href="assets/css/login.css"> -->
+</head>
+<body>
---
+++ include/nav.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Grocery</a>
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+        <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
